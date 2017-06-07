95a646becb110d4bced545f58a0ef9600f076054
Fabian Dietenberger <piano.fabian@t-online.de>
2017-06-07 19:57:43 +0200

    Put template into app

--- src/app/components/app.js
+++ src/app/components/app.js
@@ -5,14 +5,30 @@
 'use strict';
 
 import React from 'react';
-import Template from './tempalte';
-import { HashRouter } from 'react-router-dom';
+import Header from './header/header';
+import SongPlayer from './player/player-panel';
+import Home from './home/home';
+import About from './about/about';
+import Imprint from './imprint/imprint';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 export default () => {
-    // TODO put everything from template component inside here
     return (
-        <HashRouter>
-            <Template />
-        </HashRouter>
+        <div>
+            <Header />
+            <div className="container">
+                <HashRouter>
+                    <Switch>
+                        <Route exact path="/about" component={ About }/>
+                        <Route exact path="/imprint" component={ Imprint }/>
+                        <Route exact path="/" component={ Home }/>
+                    </Switch>
+                </HashRouter>
+            </div>
+            <div className="bottom">
+                { /* TODO: put this in own bottom component */ }
+                <SongPlayer />
+            </div>
+        </div>
     );
 };
